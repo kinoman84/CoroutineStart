889365036149b2ead12b121472ec602163ac8ecc
kinoman <a.buchnev.kinoman@gmail.com>
2022-12-18 17:53:16 +0300

    async{}

--- app/src/main/java/ru/buchnev/example/coroutinestart/MainActivity.kt
+++ app/src/main/java/ru/buchnev/example/coroutinestart/MainActivity.kt
@@ -1,16 +1,14 @@
 package ru.buchnev.example.coroutinestart
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.Handler
-import android.os.ProxyFileDescriptorCallback
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.isVisible
 import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.async
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import ru.buchnev.example.coroutinestart.databinding.ActivityMainBinding
-import kotlin.concurrent.thread
 
 class MainActivity : AppCompatActivity() {
 
@@ -25,14 +23,16 @@ class MainActivity : AppCompatActivity() {
             binding.progressBar.isVisible = true
             binding.buttonLoad.isEnabled = false
 
-            val jobCity = lifecycleScope.launch {
+            val deferredCity = lifecycleScope.async {
                 val city = loadCity()
                 binding.tvLocation.text = city
+                city
             }
 
-            val jobTemperature = lifecycleScope.launch {
+            val deferredTemperature = lifecycleScope.async {
                 val temp = loadTemperature()
                 binding.tvTemperature.text = temp.toString()
+                temp
             }
 
             /**
@@ -40,10 +40,15 @@ class MainActivity : AppCompatActivity() {
              * и отрабатывает код по скрытию лоадера
              */
             lifecycleScope.launch {
-                jobCity.join()
-                jobTemperature.join()
+                val city = deferredCity.await()
+                val temp = deferredTemperature.await()
                 binding.progressBar.isVisible = false
                 binding.buttonLoad.isEnabled = true
+                Toast.makeText(
+                    this@MainActivity,
+                    "City: $city, temperature: $temp",
+                    Toast.LENGTH_SHORT
+                ).show()
             }
         }
     }
